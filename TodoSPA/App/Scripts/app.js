6b1eb2a9abee648c17fd2aa66a6606e48e755690
Priya Mohanram <priyamo@microsoft.com>
2018-06-14 01:14:20 -0700

    Update sample to VS 2017, update the dependencies and README.md (#16)
    * Update sample to VS 2017, update the dependencies and README.md
    
    * Update ADAL to 1.0.17
    
    * Remove unnecessary files that were added by Nuget
    
    * Adding App creation scripts to test the sample quickly

--- TodoSPA/App/Scripts/app.js
+++ TodoSPA/App/Scripts/app.js
@@ -7,7 +7,7 @@
         tenant: '[Enter your tenant here, e.g. contoso.onmicrosoft.com]',
         clientId: '[Enter your client_id here, e.g. g075edef-0efa-453b-997b-de1337c29185]',
         postLogoutRedirectUri: window.location.origin,
-        cacheLocation: 'localStorage', // enable this for IE, as sessionStorage does not work for localhost.
+        cacheLocation: 'localStorage' // enable this for IE, as sessionStorage does not work for localhost.
     };
     var authContext = new AuthenticationContext(config);
 
@@ -89,7 +89,7 @@
         $.ajax({
             type: "GET",
             url: "App/Views/" + view + '.html',
-            dataType: "html",
+            dataType: "html"
         }).done(function (html) {
 
             // Show HTML Skeleton (Without Data)
@@ -103,7 +103,7 @@
         }).always(function () {
 
         });
-    };
+    }
 
     function stripHash(view) {
         return view.substr(view.indexOf('#') + 1);
